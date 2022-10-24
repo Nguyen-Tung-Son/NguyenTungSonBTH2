e552b92cdaab08f21fd412c270246464fca2c380
Nguyen Tung Son <titlai434321@gmail.com>
2022-10-24 21:32:32 +0700

    them du lieu Student

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using DemoMVC2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMVC2.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students {get; set;}
+    }
+}

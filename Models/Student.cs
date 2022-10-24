6fcb9c227c89b8f198eb98f016fbfc5664a78639
Nguyen Tung Son <titlai434321@gmail.com>
2022-10-24 21:08:10 +0700

    test3

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+using System;
+namespace DemoMVC2.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
